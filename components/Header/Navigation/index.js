f0ec06f2d1b3548be2734c3651703ab79f23cbfb
syennings <syhennings@live.com>
2023-12-04 17:57:15 +0100

    change positioning

--- components/Header/Navigation/index.js
+++ components/Header/Navigation/index.js
@@ -3,7 +3,7 @@ import styles from "./navigation.module.css";
 import { useState } from "react";
 
 export function Navigation() {
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleToggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
