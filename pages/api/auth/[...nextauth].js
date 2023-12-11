681d43d7229b80c989cf61ea7a38fef19778a9b1
syennings <syhennings@live.com>
2023-12-11 18:01:42 +0100

    secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,6 +13,7 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      secret: process.env.SECRET,
     }),
     // ...add more providers here
   ],
